ba8786944a139b4047ebd1a84dcde48c2878257b
Suhel Rana <suhelrana53@gmail.com>
2024-12-03 17:50:53 +0600

    initial commit

--- omod/src/main/java/com/test/omod/OmodApplication.java
+++ omod/src/main/java/com/test/omod/OmodApplication.java
@@ -1,12 +1,12 @@
-package com.test.omod;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = {"com.test.api"})
-public class OmodApplication {
-    
-    public static void main(String[] args) {
-        SpringApplication.run(OmodApplication.class, args);
-    }
-}
+//package com.test.omod;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication(scanBasePackages = {"com.test.api"})
+//public class OmodApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(OmodApplication.class, args);
+//    }
+//}
